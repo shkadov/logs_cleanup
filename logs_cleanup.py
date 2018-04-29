7f7e65ed9f38bac3155f20e935bb7b180e0224d3
shkadov <PerfaV0RE>
2018-04-29 10:15:46 +0300

    changed > to <

--- logs_cleanup.py
+++ logs_cleanup.py
@@ -34,7 +34,7 @@ def zipping_file(file_path):
                 file_date = os.path.getmtime(file_name)
                 file_age = (int(current_time) - int(file_date)) / 86400 # get current file age
                 if fnmatch.fnmatch(file_name, '*[!.zip]'):
-                    if file_age > old_date:
+                    if file_age < old_date:
                         zipfilename = file_name + '.zip'
                         file_zip = zipfile.ZipFile(zipfilename, 'w')
                         file_zip.write(file_name, compress_type=compression)
