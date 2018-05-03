fbab9a3da56c8cbcf2a5fb883963fd0341b80d1a
shkadov <PerfaV0RE>
2018-05-03 10:06:04 +0300

    corrected zip file path

--- zipping.py
+++ zipping.py
@@ -4,7 +4,7 @@ import os
 import zipfile
 import logging
 import datetime
-
+from os.path import basename
 from logs_cleanup import current_time, old_date, log_file
 
 try:
@@ -26,13 +26,11 @@ def zipping_file(file_path):
                 file_date = os.path.getmtime(file_name)
                 file_age = (int(current_time) - int(file_date)) / 86400 # get current file age
                 old_date = int(old_date)
-                if file_age < old_date:
-                    print("File age more than " + str(file_age-old_date))
                 if fnmatch.fnmatch(file_name, '*[!.zip]'):
                     if file_age > old_date:
                         zipfilename = file_name + '.zip'
                         file_zip = zipfile.ZipFile(zipfilename, 'w')
-                        file_zip.write(file_name, compress_type=compression)
+                        file_zip.write(file_name, basename(file_name), compress_type=compression)
                         with open(log_file, 'a') as f:
                             f.write(str(datetime.datetime.now()) + ' File ' + str(file_name) + ' has been compressed\n')
                             f.close()
